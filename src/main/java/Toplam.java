ad30e473c1288cec239f54b5655b117a48a7adaa
Ozlem Sert <ozlemksert@gmail.com>
2023-03-12 20:46:20 +0100

    v4

--- src/main/java/Toplam.java
+++ src/main/java/Toplam.java
@@ -2,6 +2,7 @@ public class Toplam {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
-        System.out.println("version 3");
+        System.out.println("version 3.");
+        System.out.println("v4");
     }
 }
