fa903b98280790bc4fdf706bd9da0a8a162b7624
Ozlem Sert <ozlemksert@gmail.com>
2023-03-12 22:22:56 +0100

    vtt

--- src/main/java/Toplam.java
+++ src/main/java/Toplam.java
@@ -2,12 +2,7 @@ public class Toplam {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
-        System.out.println("version 3.");
-        System.out.println("v4");
-        // v5 master kodlari
-        // v6 kodlari
-        //stash deneme kodu...
-        //restore stage
-
+        System.out.println("version 3");
+        //
     }
 }
